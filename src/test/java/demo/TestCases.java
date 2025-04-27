d4178bd899fc0fe42be70f1819124ec0c5b39143
Paras Saini <parassaini98@gmail.com>
2025-04-27 15:11:56 +0530

    ME_QA_XYOUTUBE_SEARCH_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
@@ -17,13 +18,14 @@ import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 import java.time.Duration;
+import java.util.List;
 import java.util.logging.Level;
 
 import demo.utils.ExcelDataProvider;
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
 
-public class TestCases extends ExcelDataProvider{ // Lets us read the data
+public class TestCases extends ExcelDataProvider { // Lets us read the data
         ChromeDriver driver;
 
         /*
@@ -31,6 +33,126 @@ public class TestCases extends ExcelDataProvider{ // Lets us read the data
          * Follow `testCase01` `testCase02`... format or what is provided in
          * instructions
          */
+
+        @Test
+        public void testCase01() throws InterruptedException {
+                boolean status = false;
+                System.out.println("Test case 01 started");
+                status = Wrappers.gotoURL(driver, "https://www.youtube.com");
+                status = Wrappers.checkURL(driver, "youtube");
+                Assert.assertTrue(status, "Testcase 01 Failed");
+
+                // Click on "About" at the bottom of the sidebar.
+                status = Wrappers.optionSelect(driver,
+                                By.xpath("//*[@id='footer']/div/a[text()='About']"));
+                Assert.assertTrue(status, "Test case 01 failed");
+
+                // Print the message on the About screen
+                status = Wrappers.getMessage(driver,
+                                By.xpath("//section[@class='ytabout__content']"));
+                Thread.sleep(2000);
+                Assert.assertTrue(status, "Test case 01 failed");
+                System.out.println("Test case 01 ended");
+        }
+
+        @Test
+        public void testCase02() throws InterruptedException {
+                SoftAssert softAssert = new SoftAssert();
+                boolean status = false;
+                System.out.println("Test case 02 started");
+                driver.navigate().back();
+                // Navigate to "Movies" tab in sidebar and in the “Top Selling” section, scroll
+                // to the extreme right.
+                status = Wrappers.sideBarOptn(driver, "Movies");
+                Assert.assertTrue(status, "Test case 02 failed");
+                Thread.sleep(2000);
+                WebElement sectionReq = Wrappers.reqSection(driver, "Top selling");
+                status = Wrappers.scrollOpt(driver, sectionReq);
+                Thread.sleep(2000);
+                Assert.assertTrue(status, "Test case 02 failed");
+
+                // Checking using Soft Assert on whether the movie is marked “A” for Mature
+                // or not and if the movie is from the given category list.
+
+                WebElement lastElement = Wrappers.lastEle(driver, sectionReq,
+                                By.xpath("//ytd-grid-movie-renderer"));
+                Thread.sleep(2000);
+                String s = Wrappers.elementText(lastElement, By.xpath(".//div[2]/p"));
+                softAssert.assertEquals(s, "A", "Movie is not marked A for Mature");
+
+                s = Wrappers.elementText(lastElement,
+                                By.xpath(".//a/span[contains(@class,'ytd-grid-movie-renderer')]"));
+                String[] category = { "Comedy", "Animation", "Drama" };
+
+                boolean check = Wrappers.checkMovieCategory(s, category);
+                Thread.sleep(2000);
+
+                softAssert.assertTrue(check, "Movie category is not from the given list");
+                Assert.assertTrue(status, "Test case 02 failed");
+                System.out.println("Test case 02 ended");
+        }
+
+        @Test
+        public void testCase03() throws InterruptedException {
+                boolean status = false;
+                SoftAssert softAssert = new SoftAssert();
+                System.out.println("Test case 03 started");
+
+                // Navigate to the "Music" tab in sidebar and in the 1st "India's Biggest Hits"
+                // section.
+                // Print the name of the playlist on the most right.
+
+                status = Wrappers.sideBarOptn(driver, "Music");
+
+                WebElement firstPlayList = Wrappers.reqSection(driver, "s Biggest Hits");
+
+                WebElement lastOne = Wrappers.lastEle(driver, firstPlayList,
+                                By.xpath(".//ytd-rich-item-renderer[not(@hidden)]"));
+
+                String s = Wrappers.elementText(lastOne, By.xpath(".//a/span"));
+                System.out.println("Most right playList: " + s);
+
+                // Soft Assert on whether the number of tracks listed is less than or equal to
+                // 50 or not.
+
+                int i = Wrappers.numberElement(lastOne, By.xpath(".//badge-shape/div[2]"));
+                System.out.println("Number of tracks listed: " + i);
+                softAssert.assertEquals(i <= 50, "True",
+                                "Number of traks listed is greater than 50");
+                Assert.assertTrue(status, "Test case 03 failed");
+                System.out.println("Test case 03 ended");
+        }
+
+        @Test
+        public void testCase04() throws InterruptedException {
+                boolean status = false;
+                SoftAssert softAssert = new SoftAssert();
+                System.out.println("Test case 04 started");
+
+                // Navigate to the "News" tab in sidebar and in the "Latest news posts"
+                // section. Print the title and body of the 3 latest news posts.
+                Thread.sleep(2000);
+                status = Wrappers.sideBarOptn(driver, "News");
+
+                Thread.sleep(5000);
+                WebElement latestNewsList = Wrappers.reqSection(driver, "Latest news posts");
+                Thread.sleep(5000);
+                List<WebElement> latestNews = Wrappers.listNews(driver,
+                                By.xpath(".//ytd-post-renderer"));
+                Thread.sleep(2000);
+                Wrappers.titleBody(driver, latestNews);
+
+                // Print the number of likes on all 3 of them.
+
+                int sumLike = Wrappers.sumLikeElements(latestNews,
+                                By.xpath(".//span[@id='vote-count-middle']"));
+
+                System.out.println("Sum of likes on all 3 of them: " + sumLike);
+
+                Thread.sleep(2000);
+                Assert.assertTrue(status, "Test case 04 failed");
+                System.out.println("Test case 04 ended");
+        }
 
         /*
          * Do not change the provided methods unless necessary, they will help in
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -8,10 +8,241 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.asserts.SoftAssert;
+
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+
+    // Method to navigate to the given URL
+    public static boolean gotoURL(WebDriver driver, String url) {
+        boolean status = false;
+        try {
+            driver.get(url);
+            status = true;
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
+        }
+        return status;
+    }
+
+    // Method to check if the URL contains the given string
+    public static boolean checkURL(WebDriver driver, String s) {
+        boolean status = false;
+        try {
+            String uRl = driver.getCurrentUrl();
+            if (uRl.contains(s.toLowerCase())) {
+                System.out.println("URL contains: " + s);
+                status = true;
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println("Exception: " + e);
+            System.out.println("URL not contains: " + s);
+        }
+        return status;
+    }
+
+    // Method to check side bar is visible or not
+    // if not visible then click on the menu button to make it visible
+    public static boolean menuVisible(WebDriver driver) {
+        WebElement menu = driver.findElement(By.xpath("//button[@aria-label='Guide']"));
+        String menuStatus = menu.getAttribute("aria-pressed");
+        boolean status = false;
+        if (menuStatus.equals("false")) {
+            menu.click();
+            status = true;
+        } else if (menuStatus.equals("true")) {
+            status = true;
+        }
+        return status;
+    }
+
+    // Method to click on the option
+    public static boolean optionSelect(WebDriver driver, By locator) {
+        boolean status = false;
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        try {
+            menuVisible(driver);
+            WebElement option = wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+            option.click();
+            status = true;
+        } catch (Exception e) {
+            System.out.println("Element not found: " + e);
+        }
+        return status;
+    }
+
+    // Method to get message on the screen
+    public static boolean getMessage(WebDriver driver, By locator) {
+        boolean status = false;
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        try {
+            List<WebElement> lis = wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(locator));
+            for (WebElement li : lis) {
+                System.out.println("Message: " + li.getText());
+            }
+            status = true;
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println("Element not found: " + e);
+        }
+        return status;
+    }
+
+    // Method to click on the side bar option
+    public static boolean sideBarOptn(WebDriver driver, String s) {
+        boolean status = false;
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        try {
+            menuVisible(driver);
+            WebElement option = wait
+                    .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@title='" + s + "']")));
+            option.click();
+            status = true;
+        } catch (Exception e) {
+            System.out.println("Element not found: " + e);
+        }
+        return status;
+    }
+
+    // Method to scroll the section to the extreme right
+    public static boolean scrollOpt(WebDriver driver, WebElement ele) {
+        boolean status = false;
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        String s = ".//div[@id='right-arrow']/ytd-button-renderer";
+        try {
+            while (wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(s))).isDisplayed()) {
+                ele.findElement(By.xpath(s)).click();
+            }
+            status = true;
+        } catch (Exception e) {
+            System.out.println("Element not found: " + e);
+        }
+        return status;
+    }
+
+    // Method to get text
+    public static String elementText(WebElement last, By locator) {
+        String text = last.findElement(locator).getText();
+        return text;
+    }
+
+    // lastEle method to find the extreme right element in the given section
+    public static WebElement lastEle(WebDriver driver, WebElement ele, By locator) {
+
+        List<WebElement> elements = ele.findElements(locator);
+        WebElement lastElem = elements.get(elements.size() - 1);
+        return lastElem;
+    }
+
+    // checkMovieCategory method to check if the movie belongs to the given category
+    public static boolean checkMovieCategory(String movieName, String[] validCategories) {
+        boolean isValidCat = false;
+        try {
+            for (String category : validCategories) {
+                if (movieName.contains(category)) {
+                    isValidCat = true;
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
+        }
+        return isValidCat;
+    }
+
+    // Find the required section in the page
+    public static WebElement reqSection(WebDriver driver, String str) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+        WebElement elements = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(
+                "//span[contains(text(),'" + str + "')]/ancestor::div[@id='dismissible']")));
+        return elements;
+    }
+
+    // Get the list of news elements in reqSection
+    public static List<WebElement> listNews(WebDriver driver, By locator) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        List<WebElement> elements = new ArrayList<>();
+        try {
+            List<WebElement> ele = wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(locator));
+            for (int i = 0; i < 3; i++) {
+                elements.add(ele.get(i));
+            }
+        } catch (Exception e) {
+            System.out.println("Element not found: " + e);
+        }
+        return elements;
+    }
+
+    // Method to get number from text of the element
+    public static int numberElement(WebElement element, By locator) {
+        int number = 0;
+        try {
+            String text = element.findElement(locator).getText().trim();
+
+            if (text.isEmpty()) {
+                return 0;
+            }
+
+            text = text.toUpperCase();
+
+            if (text.endsWith("K")) {
+                number = (int) (Double.parseDouble(text.replace("K", "")) * 1000);
+            } else if (text.endsWith("M")) {
+                number = (int) (Double.parseDouble(text.replace("M", "")) * 1000000);
+            } else if (text.endsWith("B")) {
+                number = (int) (Double.parseDouble(text.replace("B", "")) * 1000000000);
+            } else {
+                // No suffix like K/M/B → directly parse
+                number = Integer.parseInt(text.replaceAll("[^0-9]", ""));
+            }
+        } catch (Exception e) {
+            System.out.println("Element not found: " + e);
+        }
+        return number;
+    }
+
+    // Method to count no of likes in 1st 3 news posts
+    public static int sumLikeElements(List<WebElement> elements, By locator) {
+        int sum = 0;
+        for (WebElement el : elements) {
+            sum += numberElement(el, locator);
+        }
+        return sum;
+    }
+
+    // Method to get the title and body of the news posts
+    public static void titleBody(WebDriver driver, List<WebElement> element) {
+        String lTitle = ".//a[@id='author-text']/span";
+        String lBody = ".//yt-formatted-string[@id='home-content-text']";
+        try {
+            int count = 1;
+            for (WebElement el : element) {
+                String title = el.findElement(By.xpath(lTitle)).getText();
+                String body;
+                // try {
+                body = el.findElement(By.xpath(lBody)).getText();
+
+                // } catch (NoSuchElementException e) {
+                // body = el.findElement(By.xpath(lBody + "/..")).getText();
+                // }
+                System.out.println(count + " Title: " + title);
+                System.out.println("Body: " + body);
+                count++;
+            }
+
+        } catch (Exception e) {
+            System.out.println("Element not found: " + e);
+        }
+
+    }
+
 }
